246b5c81586832127832da007928a9233bcb5592
andydarko <80250074+andydarko@users.noreply.github.com>
2021-05-13 17:23:16 +0200

    Add files via upload

---
+++ scripts/background.js
@@ -0,0 +1,25 @@
+let toggle = false;
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(){
+
+  if (toggle == false){
+    toggle = true;
+    chrome.browserAction.setIcon({path: "icons/19_on.png"});
+  }else{
+    toggle = false;
+    chrome.browserAction.setIcon({path: "icons/19_off.png"});
+  }
+
+  blocker(toggle);
+}
+
+function blocker(){
+  chrome.webRequest.onBeforeRequest.addListener(function (e){
+    return {cancel: toggle};
+  }, {
+    types: ["xmlhttprequest"],
+    urls: ["https://www.instagram.com/stories/reel/seen"],
+  },["blocking"]);
+}
